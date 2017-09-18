7cba76049c89eb3f71b2c5eaf946c7e2681a863b
alunopuvr <alunopuvr@UD531550.(none)>
2017-09-18 10:56:26 -0300

    LU

--- tarefa4.c
+++ tarefa4.c
@@ -35,66 +35,12 @@ void imprime(double **M, int dim)
  	
 }
 
-void decomposicaoLU(double **M, int dim)
-{	double **L, **U;
-	int i,j,k;
-	float soma1, soma2;
-   
-	L = malloc( dim*sizeof(double*));
-	U = malloc( dim*sizeof(double*));
-     
-     
-    	for(i=0;i<dim;i++)
-    	{  	
-		L[i] = malloc((dim+1)*sizeof(double));	
-       		L[i][0] = M[i][0];
-    	}
-    
-    	for(i=0;i<dim;i++)
-    	{	for(j=0;j<dim;j++)
-    	 	{	
-			U[i] = malloc((dim+1)*sizeof(double));	
-    	      		
-			if(i==j)
-    	 	  	{  U[i][i] = 1;
-    	 	  	}
-    	      		else
-    	      		{   
-				U[0][j] = M[i][j]/L[0][0];
-   	     		}    
-    	 	}
-    	}
-    
-	for(i=1;i<dim;i++)
-	{	for(j=1;j<dim;j++)
-    		{		 
-    	        	for(k=0;k<j-1;k++)
-    	        	{   	soma1 = soma1 + U[i][k]*L[k][i];
-    	            		soma2 = soma2 + U[k][i]*L[i][k];
-    	        	}
-    	        	
-			L[i][j] = M[i][j] - soma1;
-    	  
-		}
-    	  
-		for(j=i+1;j<dim;j++)
- 	   	{     
-		   	U[i][j] = (M[i][j] - soma2)/L[i][i];
-    	  	}
-   	}
-	
-	printf ("\nA matriz L é:\n");
-	imprime(L, dim);
-	printf ("\nA matriz U é:\n");
-	imprime(U, dim);
-}
-
 void escalonamento(double **M, int dim)
 {
 	int i, j, k;
 	double pivo;
 	
-	for(k=0; k<dim-1; v++)
+	for(k=0; k<dim-1; k++)
 	{
 		for(i=k+1; i<dim; i++)
 		{
@@ -105,6 +51,55 @@ void escalonamento(double **M, int dim)
 		}	
 	}
 	
+}
+
+void decomposicaoLU(double **M, int dim)
+{	double **L, **U;
+    	int i,j,k;
+    	float soma1, soma2;
+    
+     	U = malloc( dim*sizeof(double*));
+     	L = malloc( dim*sizeof(double*));
+     
+    	for(i=0;i<dim;i++)
+    	{	
+    		L[i] = malloc((dim)*sizeof(double));	
+       		L[i][0] = M[i][0];
+    	}
+    	//até aqui, L está correta!
+    
+    	i=0;
+    	for(j=0;j<dim;j++)
+    	{   
+        	U[i] = malloc((dim)*sizeof(double)); 
+        	if(i==j)
+    		{	U[i][j] = 1.0;
+    	  	 	i++;
+    		}
+        	U[0][j] = M[0][j]/L[0][0];
+        } 
+    
+   	soma1=0; soma2=0;
+    	for(i=1;i<dim;i++)
+    	{	for(j=1;j<dim;j++)
+    	  	{		 
+    	        	for(k=0;k<j-1;k++)
+    	        	{	soma1 = soma1 + U[i][k]*L[i][k];	}
+    	        L[i][j] = M[i][j] - soma1;
+    	  	}
+    	  	
+    	  	for(j=i+1;j<dim-1;j++)
+    	 	{     
+    	      		for(k=0;k<i-1;i++)
+    	      		{	soma2 = soma2 + U[k][j]*L[i][k]; 	} 
+	      	U[i][j] = (M[i][j] - soma2)/L[i][i];
+	      	}
+   	}
+	
+	printf ("\nA matriz L é:\n");
+	imprime(L, dim);
+	printf ("\nA matriz U é:\n");
+	imprime(U, dim);
 }
 
 int main()
@@ -121,9 +116,9 @@ int main()
 	printf ("\nA matriz fornecida é:\n");
 	imprime(M, dim);
 
- 	escalonamento( M, dim);
-	printf ("\nA matriz após o escalonamento é:\n");
-	imprime(M, dim);
+ 	//escalonamento( M, dim);
+	//printf ("\nA matriz após o escalonamento é:\n");
+	//imprime(M, dim);
 
 	decomposicaoLU( M, dim);
 
